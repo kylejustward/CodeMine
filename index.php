62bd6ffac8dc94eb5672095e040141df4cabcc1f
kylejustward <smmaygunlog@miners.utep.edu>
2016-09-28 14:37:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php 
+ $principleAmount = 0.0;
+ $interestRate = 0.0;
+ $numOfYears = 0.0;
+ $monthlyPayments = 0.0;
+ $totalAmountOwed = 0.0;
+?>
 <!DOCTYPE HTML>
 <html>
   <head>
-    <title></title>
+    <title>Morgage Calculator</title>
   </head>
   <body>
-    
+    <form action = "$_SERVER['PHP_SELF']" method = "post">
+      Morgage Amount<input type = "number" name = "principleAmount">                                                
+    </form>
   </body>
 </html>
