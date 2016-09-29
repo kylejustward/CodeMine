cb2662d7cb55b1b871e907cbfe9680b3c4dadebb
kylejustward <smmaygunlog@miners.utep.edu>
2016-09-28 23:58:57 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php 
- $principleAmount = 0.0;
- $interestRate = 0.0;
- $numOfYears = 0.0;
- $monthlyPayments = 0.0;
- $totalAmountOwed = 0.0;
+$principleAmount = 1.0;
+$interestRate = 1.0;
+$years = 1.0;
+$months = 1.0;
+$monthlyPayments = 1.0;
 ?>
 <!DOCTYPE HTML>
 <html>
   <head>
+    <title></title>
     <title>Morgage Calculator</title>
   </head>
   <body>
-    <form action = "$_SERVER['PHP_SELF']" method = "post">
-      Morgage Amount<input type = "number" name = "principleAmount">                                                
+    <form action = " main.php " method = "POST"><br>
+      Morgage Amount<input type = "number" name = "principleAmount"> <br>
+      Interest Rate<input type = "number" name = "interestRate"><br>
+      Length of Loan in Years<input type = "number" name = "years"><br>
+      <input type="submit" name="submit" value="CalculatePayments">                                               
     </form>
+
   </body>
 </html>
--- main.php
+++ main.php
@@ -1,3 +1,12 @@
-<?php
- echo "this is it boys!";
-?>
+    <?php 
+    	$principleAmount = $_POST['principleAmount'];
+    	$interestRate = $_POST['interestRate'];
+    	$interestRate = $interestRate / 100;
+    	$years = $_POST['years'];
+    	$months = $years * 12;
+    	$monthlyPayments = $principleAmount * (($interestRate * pow((1 + $interestRate),$months)) / (pow((1 + $interestRate),$months)-1));
+    	
+    	if(isset($_POST['principleAmount'])){
+    	echo "$principleAmount";
+    	}
+    ?>
