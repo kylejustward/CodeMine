1410d3290d3a61ba5ef16c6f8ad2764f5958b64b
kylejustward <smmaygunlog@miners.utep.edu>
2016-09-29 22:30:45 -0600

    Add files via upload

---
+++ invest.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+	<form>
+		Address<input type = "text" name = "address">
+		Square Feet<input type = "number" name = "squareFeet">
+		Price<input type = "number" name = "price">
+		Price Per Square Feet<p name="pricePerSquareFeet" id="pricePerSquareFeet"></p>
+		Closing Cost<p name="closingCost" id="closingCost"></p>
+		Agents Fee<p name="agentFee" id="agentFee"></p>
+		Cost of Repairs<p name="repairs" id="repairs"></p>
+		<hr>
+		Net<p name="net" id="net"></p>
+		Closing Cost<p name="closingCost" id="closingCost"></p>
+		Holding Cost<p name="holdingCost" id="holdingCost"></p>
+		Profit<p name="profit" id="profit"></p>
+		Offer<p name="offer" id="offer"></p>		
+	</form>
+</html>
